eb1c745c72485c67372af0cbf4e466bff503209e
Kari Ikonen <mr.kari.ikonen@gmail.com>
2022-04-10 00:20:24 +0300

    ADD initial models

---
+++ app/models/exif_value.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class ExifValue < ApplicationRecord
+  belongs_to :image
+end
---
+++ app/models/image.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class Image < ApplicationRecord
+  belongs_to :upload
+  has_many :exif_values
+end
